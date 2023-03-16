b0d48847a410adf3d36600bfef5dc9a4d537f4e3
AyobamiAdebesin <froshofficial@gmail.com>
2023-03-16 15:34:09 +0100

    Finished designing sign in and sign up pages

--- frontend/src/components/authentication/SignIn.js
+++ frontend/src/components/authentication/SignIn.js
@@ -1,11 +1,83 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  VStack,
+} from "@chakra-ui/react";
+import axios from "axios";
+import React, { useState } from "react";
 
 const SignIn = () => {
-  return (
-    <div>
-      <h1>Sign in</h1>
-    </div>
-  )
-}
+  // State variables
+  const [show, setShow] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+  // Inverts the value of show
+  // This will be passed to the onClick event of the button
+  // to toggle the visibility of the password
+  const handleClick = () => setShow(!show);
+  const submitHandler = () => {
 
-export default SignIn
+  }
+  return (
+    <VStack spacing={"5px"} color="black">
+      <FormControl id="first-name" isRequired>
+        <FormLabel>Username</FormLabel>
+        <Input
+          type="text"
+          placeholder="Username"
+          onChange={(e) => setName(e.target.value)}
+        />
+      </FormControl>
+      <FormControl id="email" isRequired>
+        <FormLabel>Email</FormLabel>
+        <Input
+          type="text"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FormControl>
+      <FormControl id="password" isRequired>
+        <FormLabel>Password</FormLabel>
+        <InputGroup>
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputRightElement width={"4.5rem"}>
+            <Button h="1.75rem" size={"sm"} onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <Button
+        width="100%"
+        style={{ marginTop: "15px", background: "#00bfa6" }}
+        onClick={submitHandler}
+      >
+        Sign In
+      </Button>
+      <Button
+      variant={"solid"}
+        width="100%"
+        style={{ marginTop: "15px", background: "grey" }}
+        onClick={() => {
+          setEmail("guest@example.com");
+          setPassword("123456");
+        }}
+      >
+        Guest User Sign In
+      </Button>
+    </VStack>
+  );
+};
+
+export default SignIn;
--- frontend/src/components/authentication/SignUp.js
+++ frontend/src/components/authentication/SignUp.js
@@ -1,14 +1,69 @@
 /* eslint-disable no-unused-vars */
-import { FormControl, FormLabel, Input, VStack } from "@chakra-ui/react";
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  VStack,
+} from "@chakra-ui/react";
+import axios from "axios";
 import React, { useState } from "react";
 
 const SignUp = () => {
+  // State variables
+  const [show, setShow] = useState(false);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [pic, setPic] = useState("");
 
+  // Inverts the value of show
+  // This will be passed to the onClick event of the button
+  // to toggle the visibility of the password
+  const handleClick = () => setShow(!show);
+
+  // Function to get the uploaded image by user
+  const postDetails = (pic) => {
+    if (pic) {
+      setPic(pic);
+    }
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
+    } else {
+      // Make a post request to the backend
+      // to create a new user
+      await axios.post("/signup", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password,
+          pic,
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.error) {
+            alert(data.error);
+          } else {
+            alert(data.message);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }
   return (
     <VStack spacing={"5px"} color="black">
       <FormControl id="first-name" isRequired>
@@ -28,13 +83,53 @@ const SignUp = () => {
         />
       </FormControl>
       <FormControl id="password" isRequired>
-        <FormLabel>Pasword</FormLabel>
+        <FormLabel>Password</FormLabel>
+        <InputGroup>
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputRightElement width={"4.5rem"}>
+            <Button h="1.75rem" size={"sm"} onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+
+      <FormControl id="confirm-password" isRequired>
+        <FormLabel>Confirm Password</FormLabel>
+        <InputGroup>
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="confirm your password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputRightElement width={"4.5rem"}>
+            <Button h="1.75rem" size={"sm"} onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <FormControl id="pic" isRequired>
+        <FormLabel>Upload your Picture</FormLabel>
         <Input
-          type="text"
-          placeholder="Username"
-          onChange={(e) => setName(e.target.value)}
+          type="file"
+          p={1.5}
+          accept="image/*"
+          onChange={(e) => postDetails(e.target.files[0])}
         />
       </FormControl>
+      <Button
+      colorScheme={"blue"}
+      width="100%"
+      style={{ marginTop: "15px", background: "#00bfa6" }}
+      onClick={submitHandler}
+      >
+        Sign Up
+      </Button>
     </VStack>
   );
 };
