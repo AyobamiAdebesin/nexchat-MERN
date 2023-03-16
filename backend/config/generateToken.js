4b3677d9f3ce6529d7ac24ec8bd6095e8165f5bf
AyobamiAdebesin <froshofficial@gmail.com>
2023-03-16 23:44:50 +0100

    Generate sign up token with jwt

---
+++ backend/config/generateToken.js
@@ -0,0 +1,10 @@
+// Function to generate a token for the user
+const jwt = require("jsonwebtoken");
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
+
+module.exports = generateToken;
