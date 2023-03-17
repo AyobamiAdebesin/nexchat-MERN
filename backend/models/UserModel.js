fb092f80a09b045196084dcad4dc14750f95a269
AyobamiAdebesin <froshofficial@gmail.com>
2023-03-17 07:29:03 +0100

    User model completed

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userModel = mongoose.Schema(
   {
@@ -17,5 +18,27 @@ const userModel = mongoose.Schema(
     timestamps: true,
   }
 );
+
+
+// This is a method that will be available on all user documents.
+// It will compare the entered password with the hashed password in the database.
+// It will return true if the passwords match, and false if they do not.
+// This method will be used in the authUser controller.
+// See backend/controllers/userControllers.js
+// for more information.
+userModel.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// This is a pre-save hook. It will run before the user is saved to the database.
+// It will hash the user's password before saving it to the database.
+userModel.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = mongoose.model("User", userModel);
 module.exports = User;
