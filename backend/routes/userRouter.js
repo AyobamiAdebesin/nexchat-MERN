287c9c1994c86d5e1fca7f6f2bdc4bf62ebd2b7d
AyobamiAdebesin <froshofficial@gmail.com>
2023-03-18 11:15:24 +0100

    Added route for querying users from db

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -7,8 +7,12 @@
 const express = require("express");
 const router = express.Router();
 const userControllers = require("../controllers/userControllers");
+const protect = require("../middleware/authMiddleware");
 
 router.route("/").post(userControllers.registerUser);
 router.post("/login", userControllers.authUser);
+
+// We protect the endpoint with the middleware
+router.get("/", protect, userControllers.getUsersWithKeyWord);
 
 module.exports = router;
