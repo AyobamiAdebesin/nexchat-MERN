62f1c7b6293be485186360669104b02f0a20384c
AyobamiAdebesin <froshofficial@gmail.com>
2023-03-19 09:05:32 +0100

    Added removeuser and adduser endpoint to the chat router

--- backend/routes/chatRouter.js
+++ backend/routes/chatRouter.js
@@ -1,25 +1,31 @@
-const express = require('express');
-const chatController = require('../controllers/chatController');
+const express = require("express");
+const {
+  accessChat,
+  fetchChats,
+  createGroupChat,
+  renameGroupChat,
+  removeUserFromGroupChat,
+  addUserToGroupChat,
+} = require("../controllers/chatController");
 const router = express.Router();
-const protect  = require('../middleware/authMiddleware');
-
+const protect = require("../middleware/authMiddleware");
 
 // For accessing the chat or creating the chat
-router.route("/").post(protect, chatController.accessChat);
+router.route("/").post(protect, accessChat);
 
 // For fetching chats from the db for that particular user
-// router.get('/', protect, fetchChats);
+router.get("/", protect, fetchChats);
 
 // For creation of a group
-// router.post('/group', protect, createGroupChat);
+router.post("/group", protect, createGroupChat);
 
 // For renaming group chats
-// router.put('/rename', protect, renameGroupChat);
+router.put("/rename", protect, renameGroupChat);
 
 // For removing users to a group chat
-// router.put('/removeuser', protect, removeUserFromGroupChat);
+router.put("/removeuser", protect, removeUserFromGroupChat);
 
 // For adding users to a group chat
-// router.put('/adduser', protect, addUserToGroupChat);
+router.put('/adduser', protect, addUserToGroupChat);
 
 module.exports = router;
