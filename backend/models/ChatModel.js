b9f50bd24b18e1741444f5ae24b882e7d425d435
AyobamiAdebesin <froshofficial@gmail.com>
2023-03-19 09:04:02 +0100

    Fixed syntax error in model definition

--- backend/models/ChatModel.js
+++ backend/models/ChatModel.js
@@ -1,26 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const chatModel = mongoose.Schema(
-    {
-        chatName: { type: String, required: true, trim: true },
-        isGroupChat: { type: Boolean, required: true, default: false },
-        users: [
-            { type: mongoose.Schema.Types.ObjectId },
-            { ref: 'User' }
-        ],
-        latestMessage: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Message'
-        },
-        groupAdmin: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User'
-        }
+  {
+    chatName: { type: String, required: true, trim: true },
+    isGroupChat: { type: Boolean, required: true, default: false },
+    users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    latestMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
     },
-    {
-        timestamps: true
-    }
+    groupAdmin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-const Chat = mongoose.model('Chat', chatModel);
+const Chat = mongoose.model("Chat", chatModel);
 module.exports = Chat;
