510089cf34c43c56d5d379162d3dfb395976441f
AyobamiAdebesin <froshofficial@gmail.com>
2023-03-22 19:53:53 +0100

    Modified the UI by adjusting the direction of the flex to separate the MyChats and ChatsBox component

--- frontend/src/pages/ChatPage.js
+++ frontend/src/pages/ChatPage.js
@@ -1,4 +1,5 @@
-import { Box } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/layout";
+import { useState } from "react";
 import ChatsBox from "../components/ChatsBox";
 import SideDrawer from "../components/miscellaneous/SideDrawer";
 import MyChats from "../components/MyChats";
@@ -6,12 +7,15 @@ import { ChatState } from "../Context/ChatProvider";
 
 const ChatPage = () => {
   const { user } = ChatState();
+  const [fetchAgain, setFetchAgain] = useState(false);
   return (
     <div style={{ width: "100%" }}>
       {user && <SideDrawer />}
-      <Box d="flex" justifyContent={"center"} w="100%" h="91.5vh" p={"10px"}>
-        {user && <MyChats />}
-        {user && <ChatsBox />}
+      <Box display="flex" justifyContent={"space-between"} flexDirection="row" width="100%" h="91.5vh" p="10px">
+        {user && <MyChats fetchAgain={fetchAgain} />}
+        {user && (
+          <ChatsBox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+        )}
       </Box>
     </div>
   );
