19dfb57a9e05ffc87c851b72dbd0b6c6a41fce98
AyobamiAdebesin <froshofficial@gmail.com>
2023-03-23 09:44:18 +0100

    Created component for displaying chats

--- frontend/src/components/MyChats.js
+++ frontend/src/components/MyChats.js
@@ -1,9 +1,14 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import { useToast } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/button";
+import { Box, Stack, Text } from "@chakra-ui/layout";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { ChatState } from "../Context/ChatProvider";
+import { AddIcon } from "@chakra-ui/icons";
+import ChatLoading from "./ChatLoading";
+import { getSender } from "../config/ChatLogic";
 
 const MyChats = ({ fetchAgain }) => {
   const [loggedUser, setLoggedUser] = useState();
@@ -19,8 +24,8 @@ const MyChats = ({ fetchAgain }) => {
       };
 
       const { data } = await axios.get("/api/chats", config);
-      setChats(data);
       console.log(data);
+      setChats(data);
     } catch (error) {
       toast({
         title: "Error Occured!",
@@ -36,7 +41,77 @@ const MyChats = ({ fetchAgain }) => {
     setLoggedUser(JSON.parse(localStorage.getItem("userInfo")));
     fetchChats();
   }, []);
-  return <>MyChats</>;
+  return (
+    <>
+      <Box
+        display={{ base: selectedChat ? "none" : "flex", md: "flex" }}
+        flexDirection="column"
+        p={3}
+        bg="lightgray"
+        w={{ base: "100%", md: "30%" }}
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Box
+          pb={3}
+          px={3}
+          fontSize={{ base: "28px", md: "25px" }}
+          fontFamily="Work Sans"
+          d="flex"
+          display="flex"
+          flexDirection="row"
+          width="100%"
+          p="10px"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          My Chats
+          <Button
+            display={"flex"}
+            flexDirection="row"
+            fontSize={{ base: "17px", md: "10px", lg: "17px" }}
+            rightIcon={<AddIcon />}
+          >
+            Create group chat
+          </Button>
+        </Box>
+        <Box
+          display={"flex"}
+          flexDirection={"column"}
+          p={3}
+          bg="lightgray"
+          w={{ base: "100% " }}
+          h="100%"
+          borderRadius="lg"
+          overflowY={"hidden"}
+        >
+          {chats ? (
+            <Stack overflowY={"scroll"}>
+              {chats.map((chat) => (
+                <Box
+                  onClick={() => setSelectedChat(chat)}
+                  bg={selectedChat === chat ? "gray.200" : "white"}
+                  color={selectedChat === chat ? "white" : "black"}
+                  px={3}
+                  py={2}
+                  borderRadius="lg"
+                  key={chat._id}
+                >
+                  <Text>
+                    {!chat.isGroupChat
+                      ? getSender(loggedUser, chat.users)
+                      : chat.chatName}
+                  </Text>
+                </Box>
+              ))}
+            </Stack>
+          ) : (
+            <ChatLoading />
+          )}
+        </Box>
+      </Box>
+    </>
+  );
 };
 
 export default MyChats;
