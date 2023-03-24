708d81bde2dedb4ce441812496f3ed0478c89bc1
AyobamiAdebesin <froshofficial@gmail.com>
2023-03-24 16:24:23 +0100

    Fixed content alignment in the UI of profile modal

--- frontend/src/components/miscellaneous/ProfileModal.js
+++ frontend/src/components/miscellaneous/ProfileModal.js
@@ -24,42 +24,50 @@ const ProfileModal = ({ user, children }) => {
       ) : (
         <IconButton d={{ base: "flex" }} icon={<ViewIcon />} onClick={onOpen} />
       )}
-      <Modal size="lg" onClose={onClose} isOpen={isOpen} isCentered>
-        <ModalOverlay />
-        <ModalContent h="410px" >
-          <ModalHeader
-            fontSize="40px"
-            fontFamily="Work sans"
-            justifyContent={"flex-center"}
-            
-          >
-            {user.name}
-          </ModalHeader>
-          <ModalCloseButton />
-          <ModalBody
-            d="flex"
-            flexDir="row"
-            alignItems="center"
-          justifyContent={"space-evenly"}
-          >
-            <Image
-              borderRadius="full"
-              boxSize="150px"
-              src={user.pic}
-              alt={user.name}
-            />
-            <Text
-              fontSize={{ base: "28px", md: "30px" }}
+      {
+        <Modal
+          closeOnOverlayClick={true}
+          size="lg"
+          onClose={onClose}
+          isOpen={isOpen}
+          isCentered
+        >
+          <ModalOverlay />
+          <ModalContent h="410px">
+            <ModalHeader
+              fontSize="40px"
               fontFamily="Work sans"
+              justifyContent={"center"}
+              display={"flex"}
             >
-              Email: {user.email}
-            </Text>
-          </ModalBody>
-          <ModalFooter>
-            <Button onClick={onClose}>Close</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+              {user.name}
+            </ModalHeader>
+            <ModalCloseButton />
+            <ModalBody
+              display="flex"
+              flexDir="column"
+              alignItems="center"
+              justifyContent={"space-evenly"}
+            >
+              <Image
+                borderRadius="full"
+                boxSize="150px"
+                src={user.pic}
+                alt={user.name}
+              />
+              <Text
+                fontSize={{ base: "28px", md: "30px" }}
+                fontFamily="Work sans"
+              >
+                Email: {user.email}
+              </Text>
+            </ModalBody>
+            <ModalFooter>
+              <Button onClick={onClose}>Close</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      }
     </>
   );
 };
