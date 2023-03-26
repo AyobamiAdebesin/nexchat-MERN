c7d1a6a8189df15fc4852761d1c5b359ba6e09d5
AyobamiAdebesin <froshofficial@gmail.com>
2023-03-26 03:40:08 +0100

    Created ChatBox component to act as a wrapper for all the chats components: Single Chat and Group chat

--- frontend/src/components/ChatsBox.js
+++ frontend/src/components/ChatsBox.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import { ChatState } from "../Context/ChatProvider";
+import SingleChat from "./SingleChat";
 
-const ChatsBox = () => {
+const ChatsBox = ({ fetchAgain, setFetchAgain }) => {
+  const { selectedChat } = ChatState();
   return (
-    <div>
-      ChatsBox
-    </div>
-  )
-}
+    <Box
+      display={{ base: selectedChat ? "flex" : "none", md: "flex" }}
+      alignItems="center"
+      flexDirection={"column"}
+      p={3}
+      bg="white"
+      w={{ base: "100%", md: "70%" }}
+      borderRadius="lg"
+      borderWidth="1px"
+    >
+      <SingleChat fetchAgain={fetchAgain} setFetchAgain={setFetchAgain}/>
+    </Box>
+  );
+};
 
-export default ChatsBox
+export default ChatsBox;
